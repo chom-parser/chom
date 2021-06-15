16b6aff87efed0279b99f1ec83adf32b64052b97
Timothée Haudebourg <timothee@haudebourg.net>
2021-06-15 20:47:25 +0200

    Generation seed.

--- src/gen/target/rust/lr0.rs
+++ src/gen/target/rust/lr0.rs
@@ -1,3 +1,12 @@
-pub fn generate(&self) -> rust_codegen::module::Ref {
-	// ...
+pub fn generate(crte: &mut rust_codegen::Crate) -> rust_codegen::module::Ref {
+	let module_ref = crte.add_module("parser");
+
+	{
+		let mut module = module_ref.borrow_mut();
+		let ty_ref = module.add_struct("Parser");
+		let mut ty = ty_ref.borrow_mut();
+		ty.add_field("lexer", lexer_ty);
+	}
+
+	module_ref
 }
