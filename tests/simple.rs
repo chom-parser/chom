6992d5c7fd9eecd54549696b298e71b4f89cb879
Timothée Haudebourg <timothee.haudebourg@hemisemi.fr>
2021-07-15 23:30:12 +0200

    Adjust tests with new evaluator

--- tests/simple.rs
+++ tests/simple.rs
@@ -24,16 +24,20 @@ fn simple() -> io::Result<()> {
 			let config = chom::gen::Config { locate: true };
 			match gen::context_from_grammar(config, grammar.mono(), &["glue"], &["ast"], &["lexer"], &["parser"]) {
 				Ok(context) => {
-					let phrase = "1 + 1";
+					use chom_ir::PrettyPrint;
+					eprintln!("{}", context.ir_context().root_module().pretty_print(context.ir_context(), "  "));
+
+					let phrase = "1 + 2";
 					let target_ty = "expr";
 					
 					let mut source = phrase.chars().map(|c| Ok(c));
-					let mut env = eval::Environment::new(context.ir_context());
+					let mut eval = eval::Evaluator::new(context.ir_context());
 					
-					let target_ty_expr = env.context().parse_ty_expr(&target_ty).expect("invalid target type expr").expect("target type not found");
-					let parser = env.context().parsers_for(&target_ty_expr).next().expect("no parser defined for target type");
+					let target_ty_expr = eval.context().parse_ty_expr(&target_ty).expect("invalid target type expr").expect("target type not found");
+					let lexer = eval.instanciate_lexer(&mut source).expect("no lexer");
+					let parser = eval.context().parsers_for(&target_ty_expr).next().expect("no parser defined for target type");
 
-					match eval::parse(&mut env, &mut source, parser) {
+					match eval.parse(parser, lexer) {
 						Ok(result) => {
 							match result {
 								Ok(value) => Ok(()),
